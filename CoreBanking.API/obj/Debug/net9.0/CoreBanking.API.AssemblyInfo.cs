4db89227540e5e90f8615170923b63c01d354b50
Mai Nguyen Hoang Vu <mainguyenhoangvu2212@gmail.com>
2025-08-21 15:04:32 +0700

    update gitignore

--- CoreBanking.API/obj/Debug/net9.0/CoreBanking.API.AssemblyInfo.cs
+++ CoreBanking.API/obj/Debug/net9.0/CoreBanking.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreBanking.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f359fa74fdeac503404630cd3976703b1a167d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7130559942dc22cb632073911c2a9bc043af1f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreBanking.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreBanking.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
